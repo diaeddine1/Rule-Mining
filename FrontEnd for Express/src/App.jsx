fd83ef82b8670d4264a858ca3549f9933939a7d8
Dia Eddine <diaeddinee@outlook.fr>
2024-05-03 18:21:41 +0100

    Commit with vercel's backend urls

--- FrontEnd for Express/src/App.jsx
+++ FrontEnd for Express/src/App.jsx
@@ -6,20 +6,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './Components/Login.jsx'
 import Get_Items from './Components/Get_Items.jsx'
 import Random_items from './Components/Random_items.jsx'
+import Container from './Components/Container.jsx'
 function App() {
   
 
   return (
   
 
-     <BrowserRouter>
-     <Navbar/>
-    <Get_Items/>
-    <Random_items/>
+      <BrowserRouter>
+        <Navbar/>
 
         <Routes>
           {/* <Route path="/" element={<Home email={email}  />} /> */}
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Container/>} />
           <Route path="/Items" element={<Get_Items/>} />
           <Route path="/RandomItems" element={<Random_items/>} />
         </Routes>
---
+++ FrontEnd for Express/src/Components/Container.jsx
@@ -0,0 +1,117 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+export default function Container() {
+    const [items, setItems] = useState([]);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [randomitems, setrandomitems] = useState([]);
+    const [username, setusername] = useState("")
+
+
+
+    const Login=async()=>{
+
+        try{
+          if(!username){
+            console.log("please give us your username")
+            return
+          }
+          const response  = await axios.post('https://rule-mining-server.vercel.app/auth/login',{
+            username:username
+    
+          })
+          //console.log(response.data.token)
+          localStorage.setItem("token",response.data.token)
+          console.log(localStorage.getItem('token'))
+          
+        }catch(error)
+        {
+          console.log(error)
+    
+        }
+       
+    
+      }
+
+
+    const fetchItems = async () => {
+        try {
+          const tokenFromStorage = localStorage.getItem('token');
+          if (!tokenFromStorage) {
+            console.log("Token not found in local storage");
+            return;
+          }
+          
+          const response = await axios.get("https://rule-mining-server.vercel.app/items/all_items", {
+            headers: {
+              Authorization: `Bearer ${tokenFromStorage}`
+            }
+          });
+          setItems(response.data);
+          console.log(response.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+
+    const fetchrandomitems = async () => {
+        try {
+          const tokenFromStorage = localStorage.getItem('token');
+          if (!tokenFromStorage) {
+            console.log("Token not found in local storage");
+            return;
+          }
+          
+          const response = await axios.get("https://rule-mining-server.vercel.app/randomitems/random_randomitems", {
+            headers: {
+              Authorization: `Bearer ${tokenFromStorage}`
+            }
+          });
+          setrandomitems(response.data);
+          console.log(response.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+  return (
+    <div className='container'>
+
+        <div className='loginform'>
+    
+            <input className='LoginInput' onChange={(e)=>setusername(e.target.value)} type='text' placeholder='Please Enter your username'></input>
+            <button className='LoginButton' onClick={Login}>Login</button>
+        </div>
+        <div className="card">
+            <h1>Get Items</h1>
+            <button className='LoginButton' onClick={fetchItems}>Get Items</button>
+            <ul style={{height:"10px"}}>
+                {items.map(item => (
+                <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+
+
+
+
+        <div className="card">
+            <h1>Random randomitems</h1>
+            <button className='LoginButton' onClick={fetchrandomitems}>Get Random randomitems</button>
+            <ul style={{height:"10px"}}>
+            {randomitems.map(item => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+            </ul>
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
