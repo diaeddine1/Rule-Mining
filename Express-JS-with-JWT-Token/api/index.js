f2a3883a841f564b9ecb07f8e5f60af710c2bdf7
Dia Eddine <diaeddinee@outlook.fr>
2024-05-03 16:38:12 +0100

    Commit with back and front end

--- Express-JS-with-JWT-Token/api/index.js
+++ Express-JS-with-JWT-Token/api/index.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/items',Route)
 app.use('/auth',auth)
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 const options = {
     definition:{
